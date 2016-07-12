384652cda5a8e63701d15197b44c108a30ad9b15
Mark Smith <aardmark.smith@gmail.com>
2016-07-12 23:49:03 +1000

    Added insert functionality

--- test/index.js
+++ test/index.js
@@ -8,3 +8,17 @@ describe('bbtree', function () {
     expect(bbtree).to.respondTo('createTree');
   });
 });
+
+describe('tree', function () {
+  describe('#insert', function () {
+    it('should insert a key value pair', function () {
+      let tree = bbtree.createTree();
+      tree.insert(1, 'one');
+      tree.insert(-1, 'minus one');
+      expect(tree.root.key).to.equal(1);
+      expect(tree.root.value).to.equal('one');
+      expect(tree.root.left.key).to.equal(-1);
+      expect(tree.root.left.value).to.equal('minus one');
+    });
+  });
+});
