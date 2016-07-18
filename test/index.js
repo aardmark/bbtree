3ec85eae6638c85d7dbcb5f7eb6c5ce90b9e21ce
Mark Smith <aardmark.smith@gmail.com>
2016-07-18 17:46:44 +1000

    Fix bug with comparer. Refactor tests.

--- test/index.js
+++ test/index.js
@@ -1,107 +1,103 @@
 'use strict';
 
-let expect = require('chai').expect;
-let bbtree = require('../lib');
+const expect = require('chai').expect;
+const bbtree = require('../lib');
+const testData = require('./testdata.json').members;
+const preOrderedTestData = require('./preOrderedData.json').members;
+const inOrderTestData = require('./inOrderData.json').members;
+const postOrderedTestData = require('./postOrderedData.json').members;
 
-describe('bbtree', function () {
+let comparer = (a, b) => {
+  return a.accountNumber - b.accountNumber;
+};
+
+describe('BbTree', function () {
   it('should export function createTree', function () {
     expect(bbtree).to.respondTo('createTree');
   });
 });
 
-describe('tree', function () {
-  describe('#insert()', function () {
-    it('should insert a key value pair', function () {
-      let tree = bbtree.createTree();
-      tree.insert(1);
-      tree.insert(-1);
-      expect(tree.get(1)).to.equal(1);
-      expect(tree.get(-1)).to.equal(-1);
+describe('Tree', function () {
+
+  let tree = null;
+  let emptyTree = bbtree.createTree();
+  let nonExistantKey = { accountNumber: 42 };
+  let validateTree = () => {
+    testData.forEach((member) => {
+      let result = tree.get({ accountNumber: member.accountNumber });
+      expect(result).to.equal(member);
     });
-    it('should not insert a duplicate key', function () {
-      let tree = bbtree.createTree();
-      tree.insert(1);
-      tree.insert(10);
-      tree.insert(1);
-      tree.insert(9);
-      tree.insert(5);
-      tree.insert(2);
-      expect(tree.count()).to.equal(5);
-      expect(tree.get(1)).to.equal(1);
+    expect(tree.count()).to.equal(testData.length);
+  };
+
+  beforeEach('Populating tree', function () {
+    tree = bbtree.createTree(comparer);
+    testData.forEach((member) => { tree.insert(member); });
+  });
+
+  describe('#insert()', function () {
+
+    it('should insert values', function () {
+      validateTree();
+    });
+
+    it('should not insert a duplicate value', function () {
+      tree.insert(testData[0]);
+      expect(tree.count()).to.equal(testData.length);
+      validateTree();
     });
   });
 
   describe('#get()', function () {
+
     it('should return null for an empty tree', function () {
-      let tree = bbtree.createTree();
-      let result = tree.get(1);
-      expect(result).to.be.a('null');
+      expect(emptyTree.get(nonExistantKey)).to.be.a('null');
     });
+
     it('should return null for a key that does not exist', function () {
-      let tree = bbtree.createTree();
-      let data = [50, 40, 30, 20, 10, 25];
-      data.forEach((value) => tree.insert(value));
-      let result = tree.get(1);
-      expect(result).to.be.a('null');
+      expect(tree.get(nonExistantKey)).to.be.a('null');
     });
+
     it('should return the value for a key that does exist', function () {
-      let tree = bbtree.createTree();
-      let data = [50, 40, 30, 20, 10, 25];
-      data.forEach((value) => tree.insert(value));
-      for (let ix = 0; ix < data.length; ix++) {
-        let result = tree.get(data[ix]);
-        expect(result).to.equal(data[ix]);
-      }
+      validateTree();
     });
   });
 
   describe('#traversePreOrder()', function () {
+
     it('should traverse the tree in pre order', function () {
-      let tree = bbtree.createTree();
-      let data = [0, 1, 2, 3, 4];
-      let preOrderedData = [1, 0, 3, 2, 4];
-      data.forEach((value) => tree.insert(value));
       let preOrder = [];
       tree.traversePreOrder((value) => { preOrder.push(value); });
-      expect(preOrder).to.deep.equal(preOrderedData);
+      expect(preOrder).to.deep.equal(preOrderedTestData);
     });
   });
 
   describe('#traverseInOrder()', function () {
+
     it('should traverse the tree in order', function () {
-      let tree = bbtree.createTree();
-      let data = [50, 40, 30, 20, 10, 25];
-      data.forEach((value) => tree.insert(value));
       let inOrder = [];
       tree.traverseInOrder((value) => { inOrder.push(value); });
-      let sortedData = data.sort((a, b) => { return a - b; });
-      expect(inOrder).to.deep.equal(sortedData);
+      expect(inOrder).to.deep.equal(inOrderTestData);
     });
   });
 
   describe('#traversePostOrder()', function () {
+
     it('should traverse the tree in post order', function () {
-      let tree = bbtree.createTree();
-      let data = [0, 1, 2, 3, 4];
-      let postOrderedData = [0, 2, 4, 3, 1];
-      data.forEach((value) => tree.insert(value));
       let postOrder = [];
       tree.traversePostOrder((value) => { postOrder.push(value); });
-      expect(postOrder).to.deep.equal(postOrderedData);
+      expect(postOrder).to.deep.equal(postOrderedTestData);
     });
   });
 
   describe('#count()', function () {
+
     it('should return zero for an empty tree', function () {
-      let tree = bbtree.createTree();
-      expect(tree.count()).to.equal(0);
+      expect(emptyTree.count()).to.equal(0);
     });
+    
     it('should return the number of nodes', function () {
-      let tree = bbtree.createTree();
-      for (let ix = 0; ix < 42; ix++) {
-        tree.insert(ix);
-        expect(tree.count()).to.equal(ix + 1);
-      }
+      expect(tree.count()).to.equal(testData.length);
     });
   });
 
