db91ae4f1e58db3a983d5690c9cd396378810df4
Mark Smith <aardmark.smith@gmail.com>
2016-07-26 14:58:52 +1000

    insert and count done

--- test/index.js
+++ test/index.js
@@ -1,15 +1,21 @@
 'use strict';
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const bbtree = require('../lib');
 const testData = require('./testdata.json').members;
 const preOrderedTestData = require('./preOrderedData.json').members;
 const inOrderTestData = require('./inOrderData.json').members;
 const postOrderedTestData = require('./postOrderedData.json').members;
 
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
 let comparer = (a, b) => {
   return a.accountNumber - b.accountNumber;
 };
+
+const C = console;
 
 describe('BbTree', function () {
   it('should export function createTree', function () {
@@ -18,143 +24,183 @@ describe('BbTree', function () {
 });
 
 describe('Tree', function () {
-
-  let tree = null;
-  let emptyTree = bbtree.createTree();
-  let nonExistantKey = { accountNumber: 42 };
+  //let tree = null;
+  //const emptyTree = bbtree.createTree();
+  //const nonExistantKey = { accountNumber: 42 };
+  /*
   let validateTree = () => {
     testData.forEach((member) => {
       let result = tree.get({ accountNumber: member.accountNumber });
-      expect(result).to.equal(member);
+      expect(result).to.eventually.equal(member);
     });
-    expect(tree.count()).to.equal(testData.length);
+    expect(tree.count()).to.eventually.equal(testData.length);
   };
-
-  beforeEach('Populating tree', function () {
-    tree = bbtree.createTree(comparer);
-    testData.forEach((member) => { tree.insert(member); });
-  });
-
-  describe('#insert()', function () {
-
-    it('should insert values', function () {
-      validateTree();
-      tree = bbtree.createTree();
-      let data = [9, 5, 10, 0, 8, 11, -1, 1, 2, 100, 101, 102, 103, 104, -45, -12, 1000, 99, -99];
-      data.forEach((value) => tree.insert(value));
-      data.forEach((value) => {
-        expect(tree.get(value)).to.equal(value);
+*/
+  let populateTree = (tree, data) => {
+    return Promise.all(
+      data.map(function (member) {
+        return tree.insert(member);
+      }));
+  };
+  /*
+    beforeEach('Populating tree', function () {
+      tree = bbtree.createTree(comparer);
+      Promise.all(
+  
+      );
+      testData.forEach((member) => {
+        tree.insert(member)
+          .then((insertedMember) => {
+            C.log(insertedMember);
+            expect(insertedMember.to.equal(member));
+          });
       });
+    });
+  */
+  describe('#insert()', function () {
+    it('should insert values with the default comparer', function () {
+      let tree = bbtree.createTree();
+      let data = [9, 5, 10, 0, 8, 11, -1, 1, 2, 100, 101, 102, 103, 104, -45, -12, 1000, 99, -99];
+      return expect(populateTree(tree, data)).to.become(data);
+    });
+
+    it('should insert values with a custom comparer', function () {
+      let tree = bbtree.createTree(comparer);
+      return expect(populateTree(tree, testData)).to.become(testData);
     });
 
     it('should not insert a duplicate value', function () {
-      tree.insert(testData[0]);
-      expect(tree.count()).to.equal(testData.length);
-      validateTree();
+      let tree = bbtree.createTree(comparer);
+      return populateTree(tree, testData)
+        .then(insertedValues => {
+          return expect(tree.insert(insertedValues[5])).to.be.rejectedWith(bbtree.BbTreeError);
+        });
+    });
+
+    it('should leave the tree intact it a duplicate value is inserted', function () {
+      let tree = bbtree.createTree(comparer);
+      return populateTree(tree, testData)
+        .then((insertedValues) => {
+          let originalRoot = tree.root;
+          return tree.insert(insertedValues[5])
+            .then(
+            () => {
+              return expect(Promise.reject('should not get here')).to.be.fulfilled;
+            },
+            () => {
+              if (tree.root === originalRoot) return Promise.resolve();
+              return Promise.reject(`Original root ${originalRoot}. Tree root ${tree.root}`);
+            });
+        });
     });
   });
 
-  describe('#get()', function () {
-
-    it('should return null for an empty tree', function () {
-      expect(emptyTree.get(nonExistantKey)).to.be.a('null');
+  /*
+    describe('#get()', function () {
+  
+      it('should return null for an empty tree', function () {
+        expect(emptyTree.get(nonExistantKey)).to.be.a('null');
+      });
+  
+      it('should return null for a key that does not exist', function () {
+        expect(tree.get(nonExistantKey)).to.be.a('null');
+      });
+  
+      it('should return the value for a key that does exist', function () {
+        validateTree();
+      });
     });
-
-    it('should return null for a key that does not exist', function () {
-      expect(tree.get(nonExistantKey)).to.be.a('null');
+  
+    describe('#traversePreOrder()', function () {
+  
+      it('should traverse the tree in pre order', function () {
+        let preOrder = [];
+        tree.traversePreOrder((value) => { preOrder.push(value); });
+        expect(preOrder).to.deep.equal(preOrderedTestData);
+      });
     });
-
-    it('should return the value for a key that does exist', function () {
-      validateTree();
+  
+    describe('#traverseInOrder()', function () {
+  
+      it('should traverse the tree in order', function () {
+        let inOrder = [];
+        tree.traverseInOrder((value) => { inOrder.push(value); });
+        expect(inOrder).to.deep.equal(inOrderTestData);
+      });
     });
-  });
-
-  describe('#traversePreOrder()', function () {
-
-    it('should traverse the tree in pre order', function () {
-      let preOrder = [];
-      tree.traversePreOrder((value) => { preOrder.push(value); });
-      expect(preOrder).to.deep.equal(preOrderedTestData);
+  
+    describe('#traversePostOrder()', function () {
+  
+      it('should traverse the tree in post order', function () {
+        let postOrder = [];
+        tree.traversePostOrder((value) => { postOrder.push(value); });
+        expect(postOrder).to.deep.equal(postOrderedTestData);
+      });
     });
-  });
-
-  describe('#traverseInOrder()', function () {
-
-    it('should traverse the tree in order', function () {
-      let inOrder = [];
-      tree.traverseInOrder((value) => { inOrder.push(value); });
-      expect(inOrder).to.deep.equal(inOrderTestData);
-    });
-  });
-
-  describe('#traversePostOrder()', function () {
-
-    it('should traverse the tree in post order', function () {
-      let postOrder = [];
-      tree.traversePostOrder((value) => { postOrder.push(value); });
-      expect(postOrder).to.deep.equal(postOrderedTestData);
-    });
-  });
-
+  */
   describe('#count()', function () {
 
     it('should return zero for an empty tree', function () {
-      expect(emptyTree.count()).to.equal(0);
+      return expect(bbtree.createTree().count()).to.eventually.equal(0);
     });
 
     it('should return the number of nodes', function () {
-      expect(tree.count()).to.equal(testData.length);
+      let tree = bbtree.createTree(comparer);
+      return populateTree(tree, testData)
+        .then(() => {
+          return expect(tree.count()).to.eventually.equal(testData.length);
+        });
     });
   });
-
-  describe('#remove()', function () {
-    
-    it('should not remove a non existant key', function () {
-      tree.remove(nonExistantKey);
-      expect(tree.count()).to.equal(testData.length);
-    });
-    
-    it('should remove the correct node', function () {
-      let size = tree.count();
-      testData.forEach((member) => {
-        let key = { accountNumber: member.accountNumber };
-        tree.remove(key);
-        expect(tree.get(key)).to.be.a('null');
-        expect(tree.count()).to.equal(--size);
+  /*
+    describe('#remove()', function () {
+  
+      it('should not remove a non existant key', function () {
+        tree.remove(nonExistantKey);
+        expect(tree.count()).to.equal(testData.length);
       });
-      expect(tree.count()).to.equal(0);
-
-      tree = bbtree.createTree();
-      [...Array(42).keys()].forEach(value => { tree.insert(value); });
-      size = tree.count();
-      for (let ix = 41; ix >= 0; ix--) {
-        tree.remove(ix);
-        expect(tree.get(ix)).to.be.a('null');
-        expect(tree.count()).to.equal(--size);
-      }
-      expect(tree.count()).to.equal(0);
-    });
-  });
-
-  describe('#find()', function () {
-    it('should return an empty array if no results are found', function () {
-      expect(tree.find((member) => { return member.lastName === 'foo'; }).length).to.equal(0);
-    });
-    it('should return the correct results', function () {
-      let lastName = 'Simpson';
-      let matcher = (member) => { return member.lastName === lastName; };
-      let results = tree.find(matcher);
-      expect(results.length).to.equal(3);
-      results.forEach((member) => {
-        expect(member.lastName).to.equal(lastName);
-      });
-      lastName = 'Gumble';
-      results = tree.find(matcher);
-      expect(results.length).to.equal(1);
-      results.forEach((member) => {
-        expect(member.lastName).to.equal(lastName);
+  
+      it('should remove the correct node', function () {
+        let size = tree.count();
+        testData.forEach((member) => {
+          let key = { accountNumber: member.accountNumber };
+          tree.remove(key);
+          expect(tree.get(key)).to.be.a('null');
+          expect(tree.count()).to.equal(--size);
+        });
+        expect(tree.count()).to.equal(0);
+  
+        tree = bbtree.createTree();
+        [...Array(42).keys()].forEach(value => { tree.insert(value); });
+        size = tree.count();
+        for (let ix = 41; ix >= 0; ix--) {
+          tree.remove(ix);
+          expect(tree.get(ix)).to.be.a('null');
+          expect(tree.count()).to.equal(--size);
+        }
+        expect(tree.count()).to.equal(0);
       });
     });
-  });
-
+  
+    describe('#find()', function () {
+      it('should return an empty array if no results are found', function () {
+        expect(tree.find((member) => { return member.lastName === 'foo'; }).length).to.equal(0);
+      });
+      it('should return the correct results', function () {
+        let lastName = 'Simpson';
+        let matcher = (member) => { return member.lastName === lastName; };
+        let results = tree.find(matcher);
+        expect(results.length).to.equal(3);
+        results.forEach((member) => {
+          expect(member.lastName).to.equal(lastName);
+        });
+        lastName = 'Gumble';
+        results = tree.find(matcher);
+        expect(results.length).to.equal(1);
+        results.forEach((member) => {
+          expect(member.lastName).to.equal(lastName);
+        });
+      });
+    });
+  */
 });
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/chai-as-promised/index.d.ts" />
 /// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
